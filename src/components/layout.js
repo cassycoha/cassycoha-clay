8e812f97d61011de5c4dd7e59bf89fcfbe698e40
cassycoha <43199196+cassycoha@users.noreply.github.com>
2025-01-05 19:03:54 -0500

    social icon enlarge, remove em dash

--- src/components/layout.js
+++ src/components/layout.js
@@ -77,7 +77,7 @@ const Layout = (props) => {
         </div>
       </main>
       <footer className="site-foot">
-        &copy; {new Date().getFullYear()} <Link to={`/`}>{title}</Link> &mdash;
+        &copy; {new Date().getFullYear()} <Link to={`/`}>{title}</Link>
       </footer>
     </div>
   );
